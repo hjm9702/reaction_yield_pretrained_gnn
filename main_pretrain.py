7814916639e56f46543b5804605490e5dd339123
Jongmin Han <68938706+hjm9702@users.noreply.github.com>
2023-07-12 11:03:10 +0900

    Update main_pretrain.py

--- main_pretrain.py
+++ main_pretrain.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     torch.manual_seed(args.seed)
     torch.backends.cudnn.benchmark = False
 
-    # We use the demo dataset (1k mols) for convenience in github repo.
+    # We use the demo dataset (10k mols) for convenience in github repo.
     # The full dataset (10M mols collected from Pubchem) can be downloaded from
     # https://arxiv.org/pdf/2010.09885.pdf
     molsuppl = Chem.SmilesMolSupplier(
